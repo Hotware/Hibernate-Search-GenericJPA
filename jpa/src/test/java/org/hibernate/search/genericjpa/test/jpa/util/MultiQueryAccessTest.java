e611df08ea9818bfabdf7dc1a0fbd64af0a8bc90
Martin Braun <martinbraun123@aol.com>
2015-08-11 22:28:19 +0200

    ..

--- jpa/src/test/java/org/hibernate/search/genericjpa/test/jpa/util/MultiQueryAccessTest.java
+++ jpa/src/test/java/org/hibernate/search/genericjpa/test/jpa/util/MultiQueryAccessTest.java
@@ -25,6 +25,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
@@ -149,7 +150,7 @@ public class MultiQueryAccessTest extends DatabaseIntegrationTest {
 					}
 					++cnt;
 				}
-				assertEquals( eventOrder.size(), cnt );
+				assertTrue("did not get the exact amount of events", eventOrder.isEmpty());
 			}
 			tx.commit();
 
