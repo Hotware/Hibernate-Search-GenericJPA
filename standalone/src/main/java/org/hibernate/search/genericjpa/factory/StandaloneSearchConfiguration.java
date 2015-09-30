cbf37d4cc073b4fa2b3cf736f44b766879efe17f
Martin Braun <martinbraun123@aol.com>
2015-09-30 14:57:41 +0200

    upgrade to Hibernate Search 5.5.0.Final. removed infinispan testing

--- standalone/src/main/java/org/hibernate/search/genericjpa/factory/StandaloneSearchConfiguration.java
+++ standalone/src/main/java/org/hibernate/search/genericjpa/factory/StandaloneSearchConfiguration.java
@@ -70,8 +70,8 @@ public class StandaloneSearchConfiguration extends SearchConfigurationBase imple
 		);
 		this.properties.computeIfAbsent(
 				"hibernate.search.lucene_version", (key) -> {
-					LOGGER.info( "defaulting to Lucene Version: " + Version.LUCENE_5_2_1.toString() );
-					return Version.LUCENE_5_2_1.toString();
+					LOGGER.info( "defaulting to Lucene Version: " + Version.LUCENE_5_3_0.toString() );
+					return Version.LUCENE_5_3_0.toString();
 				}
 		);
 		this.reflectionManager = new JavaReflectionManager();
